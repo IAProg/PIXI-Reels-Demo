802b1ebf0287a39e135811d2c60607087e046dd9
Alex <alexheppell@protonmail.com>
2025-03-22 00:31:51 +0000

    convert to ticker based project

--- src/application.ts
+++ src/application.ts
@@ -1,83 +1,57 @@
-import { Application, Point, Rectangle, Sprite, Texture } from "pixi.js";
+import { Application, Container, Renderer, Ticker} from "pixi.js";
 import { appConfig } from "./config";
 import { Background } from "./components/background";
-import { Container2d, Sprite2d} from 'pixi-projection';
-import { getTexture } from "./asset-loader";
-import { QuadContainer } from "./quadContainer";
 
 
 /**
  * The core of the application. 
  * The application is responsible for managing sub components and conducting high level logic flow
  */
-export class App extends Application {
+export class App {
     private _bg: Background;
+    private _renderer: Renderer;
+    private _stage: Container;
 
-    private _QC: QuadContainer;
-
-    constructor( ){
-        super(appConfig.canvas)
+    constructor(){
+        // create rendering context
+        this._renderer = new Renderer({
+            width: 800,
+            height: 600,
+            backgroundColor: 0x1099bb,
+            resolution: window.devicePixelRatio || 1,
+            autoDensity: true,
+        });
+        
+        document.body.appendChild(this._renderer.view);
+        this._stage = new Container();
+        
+        // create elements
         this._bg = new Background();
+        this._stage.addChild(this._bg);
 
-        const tex = getTexture("turtle");
-        const containerSprite = new Sprite2d( tex as any );
-        containerSprite.anchor.set(0.5);
-        containerSprite.height = 400;
-        containerSprite.width = 400;
-
-        const containerSprite2 = new Sprite2d( tex as any );
-        containerSprite2.anchor.set(0.5);
-        containerSprite2.height = 50;
-        containerSprite2.width = 50;
-        containerSprite2.x = 250;
-
-        const containerSprite3 = new Sprite2d( tex as any );
-        containerSprite3.anchor.set(0.5);
-        containerSprite3.height = 50;
-        containerSprite3.width = 50;
-        containerSprite3.y = 250;
-
-        const containerSprite4 = new Sprite2d( tex as any );
-        containerSprite4.anchor.set(0.5);
-        containerSprite4.height = 50;
-        containerSprite4.width = 50;
-        containerSprite4.y = -250;
-
-        
-
-        this._QC = new QuadContainer(400, 400)
-        this._QC.addChild(containerSprite, containerSprite2, containerSprite3, containerSprite4);
-
-        this._QC.controlPoints[0].x *= 0.5;
-        this._QC.controlPoints[1].x *= 0.5;
-        this._QC.controlPoints[0].y *= 0.5;
-        this._QC.controlPoints[1].y *= 0.5;
-        this._QC.updateProjection();
-        
-        this.stage.addChild(this._bg, this._QC);
-        this.scaleContent(this.screen.width, this.screen.height);
-        
-        
-        //  this._mainScene.convertTo3d();
-        
-        // listen for window resize wait a frame so content scales after renderer
+        // scale content to fit window
+        this.scaleContent(window.innerWidth, window.innerHeight);
         window.addEventListener("resize", () => 
-            requestAnimationFrame(() => 
-                this.scaleContent(this.screen.width, this.screen.height)
-            )
+            this.scaleContent(window.innerWidth, window.innerHeight)
         );   
-        
-        this.ticker.add(function(delta) {
-            //
-        }, this);
+
+        // start
+        const ticker = new Ticker();
+        ticker.add(() => {
+            this._renderer.render(this._stage);
+        });
+        ticker.start();
     }
     
     /**
      * call resize handler on components 
     */
    private scaleContent(width: number, height: number): void{
-       this._bg.resize(width, height);
-       this._QC.x = width / 2;
-       this._QC.y = height / 2;
+        this._renderer.resize(width, height);
+        this._bg.resize(width, height);
     }
 }
+
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { loadAssets, loadShapeData } from "./asset-loader";
 // pixiapp is registered for pixi inspector
 loadAssets().then(() => 
     loadShapeData().then(() => {
-        const app = new App();
-        (globalThis as any).__PIXI_APP__ = app;
-        document.body.appendChild(app.view)
+        new App();
     })
 );
 
